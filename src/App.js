d7fc079e3abb112423879cf76c42e986e1bbf675
bashduude <itsgene94@gmail.com>
2018-01-30 17:08:17 +0300

    too late

--- src/App.js
+++ src/App.js
@@ -7,33 +7,56 @@ import Image4 from './imgs/4-img.png';
 import Image5 from './imgs/5-img.png';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeSlide: true,
+      activeSlideClass: "slideractive",
+
+    } //end of state
+
+  } //end of constructor
+
   render() {
+
+
+
     return (
       <div className="daddy">
-        <div className="row row1">
-          <div className="slidewraper slidewraper1">
-            <div className="slide slide1"><img src={Image1} alt="slide1" /></div>
+        <div className="slider" id="slider">
+
+          <div className="row row1">
+            <div className="slidewraper slidewraper1">
+              <div className="slide slide1"><img src={Image1} alt="slide1" /></div>
+            </div>
           </div>
-        </div>
-        <div className="row row2">
-          <div className="slidewraper slidewraper2">
-            <div className="slide slide2"><img src={Image2} alt="slide2" /></div>
+
+          <div className="row row2">
+            <div className="slidewraper slidewraper2">
+              <div className="slide slide2"><img src={Image2} alt="slide2" /></div>
+            </div>
           </div>
-        </div>
-        <div className="row row3">
-          <div className="slidewraper slidewraper3">
-            <div className="slide slide3"><img src={Image3} alt="slide3" /></div>
+
+          <div className="row row3">
+            <div className="slidewraper slidewraper3">
+              <div className="slide slide3"><img src={Image5} alt="slide3" /></div>
+            </div>
+
+            <div className="slidewraper slidewraper3">
+              <div className="slide slide4"><img src={Image4} alt="slide4" /></div>
+            </div>
+
+            <div className="slidewraper slidewraper3">
+              <div className="slide slide5"><img src={Image3} alt="slide5" /></div>
+            </div>
           </div>
-          <div className="slidewraper slidewraper3">
-            <div className="slide slide4"><img src={Image4} alt="slide4" /></div>
-          </div>
-          <div className="slidewraper slidewraper3">
-            <div className="slide slide5"><img src={Image5} alt="slide5" /></div>
-          </div>
+
         </div>
       </div>
-    );
-  }
-}
+    ); //end of return
+  } //end of render()
+} //end of App component
 
 export default App;
